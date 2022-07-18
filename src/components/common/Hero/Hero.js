fcdebb49ffcae3258709e8ec539f4ca06f7f81d3
alvaroid01 <alvaro.iglesias.diaz.13@gmail.com>
2022-07-19 01:02:43 +0200

    Link to CV pdf file

--- src/components/common/Hero/Hero.js
+++ src/components/common/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.sass'
 import profilePicture from '../../../assets/images/pfp.jpg'
+import engCV from '../../../assets/files/CV_ENG.pdf'
 import Skills from '../SkillsLayout/Skills'
 const Hero = () => {
     return (
@@ -13,16 +14,16 @@ const Hero = () => {
                     <p className='sub-text'>I'm a <b>Software Developer</b>. I mainly develop using React.js as frontend framework although I ocassionally
                         build fullstack aplications using Node and Express. <br />
                         I love <b>creating software and web aplications</b> as much as engineering my way throught problems.
-                        This coding journey of mine started 3 years ago when upon looking for careers I decided to,
-                        instead of going for a uni degree like all my other collegues, getting myself into a 2 years web and app programming course.
+                        This journey began 3 years ago, when upon looking for careers, I decided to,
+                        instead of going for a university degree like all my other collegues, getting myself into a 2 years web and app programming course.
                         Ever since that very moment I haven't stop <b>learning each and every day</b> aiming to become a <b>better developer</b>.</p>
-                    <button><a href='#projects'>My projects</a></button>
+                    <button><a href={engCV} target="_blank" rel="noreferrer">My CV</a></button>
                 </div>
                 <div className='hero-image'>
                     <img src={profilePicture} />
                 </div>
             </div>
-           
+
         </div>
     )
 
